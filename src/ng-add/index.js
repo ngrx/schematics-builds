1fbe6b9b2455d1ce28403d48b730e44f9c9bbe53
GitHub <noreply@github.com>
2020-10-07 20:27:07 +0000

    b06084af docs(effects): update steps (#2727)

--- src/ng-add/index.js
+++ src/ng-add/index.js
@@ -1,22 +1,11 @@
 "use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 exports.__esModule = true;
 var schematics_1 = require("@angular-devkit/schematics");
-var schematics_core_1 = require("../../schematics-core");
-function updateWorkspaceCli(host, value) {
-    var workspace = schematics_core_1.getWorkspace(host);
-    var path = schematics_core_1.getWorkspacePath(host);
-    workspace['cli'] = __assign(__assign({}, workspace['cli']), value);
+var config_1 = require("../../schematics-core/utility/config");
+function updateWorkspace(host, key, value) {
+    var workspace = config_1.getWorkspace(host);
+    var path = config_1.getWorkspacePath(host);
+    workspace[key] = value;
     host.overwrite(path, JSON.stringify(workspace, null, 2));
 }
 function setAsDefaultSchematics() {
@@ -24,7 +13,7 @@ function setAsDefaultSchematics() {
         defaultCollection: '@ngrx/schematics'
     };
     return function (host) {
-        updateWorkspaceCli(host, cli);
+        updateWorkspace(host, 'cli', cli);
         return host;
     };
 }
