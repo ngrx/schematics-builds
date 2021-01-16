b2d1c94650bd370c96ce7c6f6bc8f7241df481d4
GitHub <noreply@github.com>
2021-01-16 21:20:48 +0000

    2f5dcb48 perf(schematics): speed up create effect migration (#2873)

---
+++ schematics-core/testing/update.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+exports.versionPrefixes = exports.upgradeVersion = void 0;
+exports.upgradeVersion = '6.0.0';
+exports.versionPrefixes = ['~', '^', ''];
+//# sourceMappingURL=update.js.map
