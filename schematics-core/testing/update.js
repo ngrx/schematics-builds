49c1ab15c80ec41b631e71a58323211c557980ea
GitHub <noreply@github.com>
2021-01-24 16:57:25 +0000

    c5a5dd19 Interface renames (#2889)

--- schematics-core/testing/update.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.versionPrefixes = exports.upgradeVersion = void 0;
-exports.upgradeVersion = '6.0.0';
-exports.versionPrefixes = ['~', '^', ''];
-//# sourceMappingURL=update.js.map
