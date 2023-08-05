5bdecace07f551f92ab88aba00c75c8d0b4039ab
GitHub <noreply@github.com>
2023-08-05 21:27:43 +0000

    19ebb0af feat(schematics): add entity generation as part of feature schematic (#3850)

--- src/feature/index.js
+++ src/feature/index.js
@@ -3,30 +3,54 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var schematics_1 = require("@angular-devkit/schematics");
 function default_1(options) {
     return function (host, context) {
-        return (0, schematics_1.chain)([
-            (0, schematics_1.schematic)('action', {
-                flat: options.flat,
-                group: options.group,
-                name: options.name,
-                path: options.path,
-                project: options.project,
-                skipTests: options.skipTests,
-                api: options.api,
-                prefix: options.prefix,
-            }),
-            (0, schematics_1.schematic)('reducer', {
-                flat: options.flat,
-                group: options.group,
-                module: options.module,
-                name: options.name,
-                path: options.path,
-                project: options.project,
-                skipTests: options.skipTests,
-                reducers: options.reducers,
-                feature: true,
-                api: options.api,
-                prefix: options.prefix,
-            }),
+        return (0, schematics_1.chain)((options.entity
+            ? [
+                (0, schematics_1.schematic)('entity', {
+                    name: options.name,
+                    path: options.path,
+                    project: options.project,
+                    flat: options.flat,
+                    skipTests: options.skipTests,
+                    module: options.module,
+                    reducers: options.reducers,
+                    group: options.group,
+                    feature: true,
+                }),
+            ]
+            : [
+                (0, schematics_1.schematic)('action', {
+                    flat: options.flat,
+                    group: options.group,
+                    name: options.name,
+                    path: options.path,
+                    project: options.project,
+                    skipTests: options.skipTests,
+                    api: options.api,
+                    prefix: options.prefix,
+                }),
+                (0, schematics_1.schematic)('reducer', {
+                    flat: options.flat,
+                    group: options.group,
+                    module: options.module,
+                    name: options.name,
+                    path: options.path,
+                    project: options.project,
+                    skipTests: options.skipTests,
+                    reducers: options.reducers,
+                    feature: true,
+                    api: options.api,
+                    prefix: options.prefix,
+                }),
+                (0, schematics_1.schematic)('selector', {
+                    flat: options.flat,
+                    group: options.group,
+                    name: options.name,
+                    path: options.path,
+                    project: options.project,
+                    skipTests: options.skipTests,
+                    feature: true,
+                }),
+            ]).concat([
             (0, schematics_1.schematic)('effect', {
                 flat: options.flat,
                 group: options.group,
@@ -39,16 +63,7 @@ function default_1(options) {
                 api: options.api,
                 prefix: options.prefix,
             }),
-            (0, schematics_1.schematic)('selector', {
-                flat: options.flat,
-                group: options.group,
-                name: options.name,
-                path: options.path,
-                project: options.project,
-                skipTests: options.skipTests,
-                feature: true,
-            }),
-        ])(host, context);
+        ]))(host, context);
     };
 }
 exports.default = default_1;
