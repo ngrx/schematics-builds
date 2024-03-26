1c3f77e7abadc8d9321c1910b9f77f35a679c1a7
GitHub <noreply@github.com>
2024-03-26 16:53:05 +0000

    092076c2 chore: update dependencies (#4277)

--- schematics-core/test-setup.js
+++ schematics-core/test-setup.js
@@ -1,4 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("jest-preset-angular/setup-jest");
+var util_1 = require("util");
+Object.assign(global, { TextDecoder: util_1.TextDecoder, TextEncoder: util_1.TextEncoder });
 //# sourceMappingURL=test-setup.js.map
